2d401daa9c08e8893d82ed31ff8e675bf5d090ee
chrisorisawayi <chris.orisawayi@hmcts.net>
2021-07-16 18:24:31 +0100

    updated locals to passing auth creds to script as arguments

--- locals.tf
+++ locals.tf
@@ -1,3 +1,3 @@
 locals {
-  cse_script  = filebase64("${path.module}/scripts/install-splunk-forwarder-service.sh")
+  cse_script  = "${path.module}/scripts/install-splunk-forwarder-service.sh ${data.azurerm_key_vault_secret.splunk_username.value} ${data.azurerm_key_vault_secret.splunk_password.value}"
 }
