1e0f93c52531cee902b1654f99e697723fe04e57
chrisorisawayi <chris.orisawayi@hmcts.net>
2021-07-18 17:22:37 +0100

    updated script url

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
   cse_script  = "./${var.script_name} ${data.azurerm_key_vault_secret.splunk_username.value} ${data.azurerm_key_vault_secret.splunk_password.value}"
-  script_uri  = "https://raw.githubusercontent.com/hmcts/terraform-module-splunk-universal-forwarder/master/scripts/${var.script_name}"
+  script_uri  = "https://raw.githubusercontent.com/hmcts/terraform-module-splunk-universal-forwarder/dtspo-3774-dynatrace-private-synthetic/scripts/${var.script_name}"
 }
