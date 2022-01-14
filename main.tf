4806664db6b82ae07170c7e9d3f108064d05d49a
taherkhan30 <taher.khan@hmcts.net>
2022-01-14 09:44:08 +0000

    DTSPO-6211 Updated module to support Windows install

--- main.tf
+++ main.tf
@@ -35,3 +35,33 @@ resource "azurerm_virtual_machine_extension" "splunk-uf" {
     }
     PROTECTED_SETTINGS
 }
+
+resource "azurerm_virtual_machine_extension" "splunk-uf-windows" {
+
+  count =  var.os_type == "Windows" ? 1 : 0
+
+  name                       = var.extension_name
+  virtual_machine_id         = var.virtual_machine_id
+  publisher                  = "Microsoft.Compute"
+  type                       = "CustomScriptExtension"
+  type_handler_version       = var.type_handler_version
+  auto_upgrade_minor_version = var.auto_upgrade_minor_version
+
+  protected_settings = <<PROTECTED_SETTINGS
+    { 
+        "commandToExecute": "powershell -command \"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('${base64encode(data.template_file.tf.rendered)}')) | Out-File -filepath \scripts\install-splunk-forwarder-service.ps1\" && powershell -ExecutionPolicy Unrestricted -File \scripts\install-splunk-forwarder-service.ps1 -username ${data.template_file.tf.vars.username} -password ${data.template_file.tf.vars.splunkPassword} -pass4symmkey ${data.template_file.tf.vars.pass4symmkey} -splunk_group ${data.template_file.tf.vars.splunk_group}"
+     
+    }
+    PROTECTED_SETTINGS
+
+}
+
+data "template_file" "tf" {
+    template = "${file("./scripts/install-splunk-forwarder-service.ps1")}"
+    vars = {
+        username                    = "${var.splunk_username}"
+        password                    = "${var.splunk_password}"
+        pass4symmkey                = "${var.splunk_pass4symmkey}"
+        splunk_group                = "${var.splunk_group}"
+  }
+}
---
+++ scripts/install-splunk-forwarder-service.ps1
@@ -0,0 +1,39 @@
+# this script downloads the Splunk Universal Forwarder and installs it in a Windows machine
+
+[CmdletBinding()]
+
+param 
+( 
+    [Parameter(ValuefromPipeline=$true,Mandatory=$true)] [string]$splunkPassword,
+    [Parameter(ValuefromPipeline=$true,Mandatory=$true)] [string]$username
+)
+
+$password = ConvertTo-SecureString -AsPlainText $splunkPassword -Force
+$msiDownload = "https://download.splunk.com/products/universalforwarder/releases/8.2.2.1/windows/splunkforwarder-8.2.2.1-ae6821b7c64b-x64-release.msi"
+$msiFile = $env:Temp + "\splunkforwarder-8.2.2.1-ae6821b7c64b-x64-release.msi"
+$msiArguments = @(
+    "DEPLOYMENT_SERVER='splunk-lm-prod-vm00.platform.hmcts.net:8089'" 
+    "RECEIVING_INDEXER='splunk-cm-prod-vm00.platform.hmcts.net:8089'"
+    "WINEVENTLOG_SEC_ENABLE=0"
+    "WINEVENTLOG_SYS_ENABLE=0" 
+    "WINEVENTLOG_APP_ENABLE=0" 
+    "AGREETOLICENSE=Yes" 
+    "SERVICESTARTTYPE=AUTO" 
+    "LAUNCHSPLUNK=1" 
+    "SPLUNKUSERNAME=$username"
+    "SPLUNKPASSWORD=$password" 
+    "/quiet"
+)
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Installing Splunk"
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+(New-Object System.Net.WebClient).DownloadFile($msiDownload, $msiFile)
+Start-Process -FilePath "c:\windows\system32\msiexec.exe" -ArgumentList '/i', "$msiFile", "$msiArguments" -Wait -Verbose
+
+
+If ((Get-Service -name splunkforwarder).Status -ne "Running") {
+    throw "Splunk forwarder service not running"
+  }
+
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Splunk installation successful"
+Write-Host "this is the" $username
+Write-Host "this is the" $splunkPassword
