b21d4881efa2fb0bcd1136f91168d25eea51a835
taherkhan30 <taher.khan@hmcts.net>
2022-01-21 05:53:39 +0000

    dtspo 6211 added logic in heredoc to choose script based on os_type

--- locals.tf
+++ locals.tf
@@ -1,5 +1,6 @@
 locals {
   cse_script = "./${var.script_name} ${var.splunk_username} ${var.splunk_password} ${var.splunk_pass4symmkey} ${var.splunk_group}"
   script_uri = "https://raw.githubusercontent.com/hmcts/terraform-module-splunk-universal-forwarder/master/scripts/${var.script_name}"
-  ps_script_uri = "https://raw.githubusercontent.com/hmcts/terraform-module-splunk-universal-forwarder/master/scripts/install-splunk-forwarder-service.ps1"
+  ps_script  = "powershell -command \"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('${base64encode(data.template_file.tf.rendered)}')) | Out-File -filepath install-splunk-forwarder-service.ps1\" && powershell -ExecutionPolicy Unrestricted -File install-splunk-forwarder-service.ps1 -username ${data.template_file.tf.vars.username} -password ${data.template_file.tf.vars.password} -pass4symmkey ${data.template_file.tf.vars.pass4symmkey} -group ${data.template_file.tf.vars.group}"
+
 }
