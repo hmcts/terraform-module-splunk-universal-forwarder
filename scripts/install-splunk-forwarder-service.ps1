ec20c36fa064bb5f59404abe9880b604277daaf4
Tom Kimber <thomas.kimber@hmcts.net>
2022-02-03 10:24:16 +0000

    Reverted to Splunk UF 8.2.2.1.

--- scripts/install-splunk-forwarder-service.ps1
+++ scripts/install-splunk-forwarder-service.ps1
@@ -11,28 +11,28 @@ param
 )
 
 $password = ConvertTo-SecureString -AsPlainText $splunkPassword -Force
-$msiDownload = "https://download.splunk.com/products/universalforwarder/releases/8.2.4/windows/splunkforwarder-8.2.4-87e2dda940d1-x64-release.msi"
-$msiFile = $env:Temp + "\splunkforwarder-8.2.4-87e2dda940d1-x64-release.msi"
-$receiver = 'splunk-cm-prod-vm00.platform.hmcts.net:9997'
+$msiDownload = "https://download.splunk.com/products/universalforwarder/releases/8.2.2.1/windows/splunkforwarder-8.2.2.1-ae6821b7c64b-x64-release.msi"
+$msiFile = $env:Temp + "\splunkforwarder-8.2.2.1-ae6821b7c64b-x64-release.msi"
+$receiver = 'splunk-cm-prod-vm00.platform.hmcts.net:8089'
 $msiArguments = @(
-    "DEPLOYMENT_SERVER=splunk-lm-prod-vm00.platform.hmcts.net:8089"
+    "DEPLOYMENT_SERVER='splunk-lm-prod-vm00.platform.hmcts.net:8089'"
     "RECEIVING_INDEXER=$receiver"
+    "WINEVENTLOG_SEC_ENABLE=1"
+    "WINEVENTLOG_SYS_ENABLE=1"
+    "WINEVENTLOG_APP_ENABLE=1"
+    "WINEVENTLOG_FWD_ENABLE=1"
+    "WINEVENTLOG_SET_ENABLE=1"
+    "AGREETOLICENSE=Yes"
     "SERVICESTARTTYPE=AUTO"
     "LAUNCHSPLUNK=1"
     "SPLUNKUSERNAME=$splunk_username"
     "SPLUNKPASSWORD=$password"
-    "WINEVENTLOG_APP_ENABLE=1"
-    "WINEVENTLOG_SEC_ENABLE=1"
-    "WINEVENTLOG_SYS_ENABLE=1"
-    "WINEVENTLOG_FWD_ENABLE=1"
-    "WINEVENTLOG_SET_ENABLE=1"
-    "AGREETOLICENSE=Yes"
     "/quiet"
 )
 Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Installing Splunk"
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 (New-Object System.Net.WebClient).DownloadFile($msiDownload, $msiFile)
-Start-Process -FilePath "c:\windows\system32\msiexec.exe REINSTALL=ALL REINSTALLMODE=vomus" -ArgumentList '/i', "$msiFile", "$msiArguments" -Wait -Verbose
+Start-Process -FilePath "c:\windows\system32\msiexec.exe" -ArgumentList '/i', "$msiFile", "$msiArguments" -Wait -Verbose
 
 If ((Get-Service -name splunkforwarder).Status -ne "Running") {
     throw "Splunk forwarder service not running"
