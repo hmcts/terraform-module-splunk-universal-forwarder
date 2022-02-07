e1410ca876054c6271418d04e25d002d7026d24c
Tom Kimber <thomas.kimber@hmcts.net>
2022-02-07 11:34:23 +0000

    Updated install-splunk-forwarder-service.ps1.

--- scripts/install-splunk-forwarder-service.ps1
+++ scripts/install-splunk-forwarder-service.ps1
@@ -12,9 +12,9 @@ param
 
 $msiDownload = "https://download.splunk.com/products/universalforwarder/releases/8.2.4/windows/splunkforwarder-8.2.4-87e2dda940d1-x64-release.msi"
 $msiFile = $env:Temp + "\splunkforwarder-8.2.4-87e2dda940d1-x64-release.msi"
-$receiver = 'splunk-cm-prod-vm00.platform.hmcts.net:8089'
+$receiver = "splunk-cm-prod-vm00.platform.hmcts.net:8089"
 $msiArguments = @(
-    "DEPLOYMENT_SERVER='splunk-lm-prod-vm00.platform.hmcts.net:8089'"
+    "DEPLOYMENT_SERVER=splunk-lm-prod-vm00.platform.hmcts.net:8089"
     "RECEIVING_INDEXER=$receiver"
     "WINEVENTLOG_SEC_ENABLE=1"
     "WINEVENTLOG_SYS_ENABLE=1"
