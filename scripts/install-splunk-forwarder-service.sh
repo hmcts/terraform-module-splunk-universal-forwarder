93a2b263184d623ec210fd326abffb90eab8aacf
Himesh <himesh.patel99@hotmail.com>
2023-07-04 11:03:59 +0100

    Delete install repetition

--- scripts/install-splunk-forwarder-service.sh
+++ scripts/install-splunk-forwarder-service.sh
@@ -26,7 +26,6 @@ chown -R splunk:splunk $SPLUNK_HOME
 apt install acl
 
 # Grant splunk user read access to logs
-apt install acl
 setfacl -R -m u:splunk:r /var/log
 
 if [  "$(systemctl is-active SplunkForwarder.service)" = "active"  ]; then
