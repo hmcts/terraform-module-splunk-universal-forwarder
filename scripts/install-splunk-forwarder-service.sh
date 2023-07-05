f158e0f768333a9c435943b12af152666e48e7b7
Himesh <himesh.patel99@hotmail.com>
2023-07-05 09:58:36 +0100

    comment v2

--- scripts/install-splunk-forwarder-service.sh
+++ scripts/install-splunk-forwarder-service.sh
@@ -1,64 +1,64 @@
 #!/bin/bash
 
-<<'DOWNLOAD_URL="https://download.splunk.com/products/universalforwarder/releases/8.2.2.1/linux/splunkforwarder-8.2.2.1-ae6821b7c64b-Linux-x86_64.tgz"
-INSTALL_FILE="splunkforwarder-8.2.2.1-ae6821b7c64b-Linux-x86_64.tgz"
-INSTALL_LOCATION="/opt"
-DEPLOYMENT_SERVER_URI="splunk-lm-prod-vm00.platform.hmcts.net:8089"
-FORWARD_SERVER_URI="splunk-cm-prod-vm00.platform.hmcts.net:8089"
-UF_USERNAME=$1
-UF_PASSWORD=$2
-UF_PASS4SYMMKEY=$3
-UF_GROUP=$4
+#DOWNLOAD_URL="https://download.splunk.com/products/universalforwarder/releases/8.2.2.1/linux/splunkforwarder-8.2.2.1-ae6821b7c64b-Linux-x86_64.tgz"
+#INSTALL_FILE="splunkforwarder-8.2.2.1-ae6821b7c64b-Linux-x86_64.tgz"
+#INSTALL_LOCATION="/opt"
+#DEPLOYMENT_SERVER_URI="splunk-lm-prod-vm00.platform.hmcts.net:8089"
+#FORWARD_SERVER_URI="splunk-cm-prod-vm00.platform.hmcts.net:8089"
+#UF_USERNAME=$1
+#UF_PASSWORD=$2
+#UF_PASS4SYMMKEY=$3
+#UF_GROUP=$4
 
-export SPLUNK_HOME="$INSTALL_LOCATION/splunkforwarder"
+#export SPLUNK_HOME="$INSTALL_LOCATION/splunkforwarder"
 
 # Create boot-start systemd user
-groupadd -f splunk
-id -u splunk >/dev/null 2>&1 || useradd splunk -g splunk
+#groupadd -f splunk
+#id -u splunk >/dev/null 2>&1 || useradd splunk -g splunk
 
 # Install splunk forwarder
-curl --retry 3 -# -L -o $INSTALL_FILE $DOWNLOAD_URL
-tar xvzf $INSTALL_FILE -C $INSTALL_LOCATION
-rm -rf $INSTALL_FILE
-chown -R splunk:splunk $SPLUNK_HOME
+#curl --retry 3 -# -L -o $INSTALL_FILE $DOWNLOAD_URL
+#tar xvzf $INSTALL_FILE -C $INSTALL_LOCATION
+#rm -rf $INSTALL_FILE
+#chown -R splunk:splunk $SPLUNK_HOME
 
 # Grant splunk user read access to logs
-sudo apt-get install acl
-setfacl -R -m u:splunk:r /var/log
+#sudo apt-get install acl
+#setfacl -R -m u:splunk:r /var/log
 
-if [  "$(systemctl is-active SplunkForwarder.service)" = "active"  ]; then
-  $SPLUNK_HOME/bin/splunk stop
-  sleep 10
-fi
+#if [  "$(systemctl is-active SplunkForwarder.service)" = "active"  ]; then
+  #$SPLUNK_HOME/bin/splunk stop
+  #sleep 10
+#fi
 
 # Create splunk admin user
-{
-cat <<EOF
-[user_info]
-USERNAME = $UF_USERNAME
-HASHED_PASSWORD = $($SPLUNK_HOME/bin/splunk hash-passwd $UF_PASSWORD)
-EOF
-} > $SPLUNK_HOME/etc/system/local/user-seed.conf
+#{
+#cat <<EOF
+#[user_info]
+#USERNAME = $UF_USERNAME
+#HASHED_PASSWORD = $($SPLUNK_HOME/bin/splunk hash-passwd $UF_PASSWORD)
+#EOF
+#} > $SPLUNK_HOME/etc/system/local/user-seed.conf
 
-$SPLUNK_HOME/bin/splunk stop
+#$SPLUNK_HOME/bin/splunk stop
 
 # Start splunk forwarder
-$SPLUNK_HOME/bin/splunk start --accept-license --no-prompt --answer-yes
+#$SPLUNK_HOME/bin/splunk start --accept-license --no-prompt --answer-yes
 
 # Set server name
-$SPLUNK_HOME/bin/splunk set servername $hostname -auth $UF_USERNAME:$UF_PASSWORD
-$SPLUNK_HOME/bin/splunk restart
+#$SPLUNK_HOME/bin/splunk set servername $hostname -auth $UF_USERNAME:$UF_PASSWORD
+#$SPLUNK_HOME/bin/splunk restart
 
 # Configure deploymentclient.conf
-{
-cat <<EOF
-[deployment-client]
+#{
+#cat <<EOF
+#[deployment-client]
 
-[target-broker:deploymentServer]
+#[target-broker:deploymentServer]
 # Settings for HMCTS DeploymentServer
-targetUri = $DEPLOYMENT_SERVER_URI
-EOF
-} > $SPLUNK_HOME/etc/system/local/deploymentclient.conf
+#targetUri = $DEPLOYMENT_SERVER_URI
+#EOF
+#} > $SPLUNK_HOME/etc/system/local/deploymentclient.conf
 
 
 # Configure outputs.conf
@@ -80,10 +80,10 @@ EOF
 # } > $SPLUNK_HOME/etc/system/local/outputs.conf
 
 # Create boot-start systemd service
-$SPLUNK_HOME/bin/splunk stop
-$SPLUNK_HOME/bin/splunk disable boot-start
-sleep 10
-$SPLUNK_HOME/bin/splunk enable boot-start -systemd-managed 1 -user splunk -group splunk
-chown -R splunk:splunk $SPLUNK_HOME
+#$SPLUNK_HOME/bin/splunk stop
+#$SPLUNK_HOME/bin/splunk disable boot-start
+#sleep 10
+#$SPLUNK_HOME/bin/splunk enable boot-start -systemd-managed 1 -user splunk -group splunk
+#chown -R splunk:splunk $SPLUNK_HOME
 
-$SPLUNK_HOME/bin/splunk start'>>
+#$SPLUNK_HOME/bin/splunk start
