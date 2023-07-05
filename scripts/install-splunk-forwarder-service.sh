7c5016edacfa5dfe537691c70d9e7465e905d021
Himesh <himesh.patel99@hotmail.com>
2023-07-05 15:12:52 +0100

    Add setfacl installer

--- scripts/install-splunk-forwarder-service.sh
+++ scripts/install-splunk-forwarder-service.sh
@@ -23,6 +23,7 @@ rm -rf $INSTALL_FILE
 chown -R splunk:splunk $SPLUNK_HOME
 
 # Grant splunk user read access to logs
+apt install acl
 setfacl -R -m u:splunk:r /var/log
 
 if [  "$(systemctl is-active SplunkForwarder.service)" = "active"  ]; then
