697fae1af0a47ff69107c75a6c1c17bdd220b112
Himesh <himesh.patel99@hotmail.com>
2023-07-06 11:29:58 +0100

    locals script_uri change branch name

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
   cse_script = "./${var.script_name} ${var.splunk_username} ${var.splunk_password} ${var.splunk_group}"
-  script_uri = "https://raw.githubusercontent.com/hmcts/terraform-module-splunk-universal-forwarder/master/scripts/${var.script_name}"
+  script_uri = "https://raw.githubusercontent.com/hmcts/terraform-module-splunk-universal-forwarder/Splunk-change/scripts/${var.script_name}"
 }
 
